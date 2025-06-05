f5a2ddcfeb8e26e36c4341679cfaf7281373b98c
“mere” <github@zoltanb.co.uk>
2025-06-05 16:14:55 +0000

    feat: mdx

--- netlify/edge-functions/upload.ts
+++ netlify/edge-functions/upload.ts
@@ -1,6 +1,18 @@
-import { getStore } from "~/utils/blob-store";
 
-export const prerender = false;
+
+import { getStore as getStoreFromNetlify } from "@netlify/blobs";
+
+export const getStore = (key: string) => {
+  if (import.meta.env.NETLIFY_SITE_ID && import.meta.env.NETLIFY_BLOBS_TOKEN) {
+  return getStoreFromNetlify({
+    name: key,
+    siteID: import.meta.env.NETLIFY_SITE_ID,
+    token: import.meta.env.NETLIFY_BLOBS_TOKEN,
+  });
+} else {
+    return getStoreFromNetlify(key);
+}
+};
 
 export default async (request, context) => {
   if (request.method !== 'POST') {
