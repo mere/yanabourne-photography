ea43c30db2348500b2931b7800cf7c5e8f8b6eb5
“mere” <github@zoltanb.co.uk>
2025-06-05 15:37:14 +0000

    feat: mdx

---
+++ netlify/edge-functions/upload.js
@@ -0,0 +1,13 @@
+import { Context } from "@netlify/edge-functions";
+
+export default async (request: Request, context: Context) => {
+
+  const joke = await fetch("https://icanhazdadjoke.com/", {
+    "headers": {
+      "Accept": "application/json"
+    }
+  });
+  
+  const jsonData = await joke.json();
+  return Response.json(jsonData);
+};
